ea6f4fc5b3513157139ac0e5c65b850b5e41e788
Nikhil Shah <email4nikhil@gmail.com>
2021-06-01 12:31:15 -0400

    finished addUser method

--- DBConnector.java
+++ DBConnector.java
@@ -3,29 +3,69 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/*
+* Author: Nikhil Shah
+* Project: Social Media Project
+* Connect Java to SQL Database
+*/
+
 class DBConnector{
 	
 	private String url;
 	private String username;
 	private String password;
+	private Connection conn;
 	
 	public DBConnector(String url, String username, String password){
 		this.url = url;
 		this.username = username;
 		this.password = password;
+		testConnect();
 	}
-	public testConnect(){
+	public void testConnect(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection(url, username, password);
+			this.conn = DriverManager.getConnection(url, username, password);
 			Statement st = conn.createStatement();
 		} catch (SQLException e){
 			System.err.println(e.toString());
 		}
 	}
-	public addUser(String newUser, String newPass, String firstName, String lastName, String email){
-		String passHash = getSHA(newPass);
-		
+	public boolean addUser(String newUser, String newPass, String firstName, String lastName, String email){
+		if (!isUser(newUser)){
+			String passHash = getSHA(newPass);
+			String sqlAddEntry = "INSERT INTO login VALUES (default, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement ps = conn.prepareStatement(sqlAddEntry);
+			ps.setString(1, newUser);
+			ps.setString(2, firstName);
+			ps.setString(3, lastName);
+			ps.setString(4, email);
+			ps.setString(5, passHash);
+			ps.setString(6, false);
+			ps.executeUpdate();
+			return isUser(newUser);
+		}
+		else{ 
+			return false;
+		}
+	}
+	private boolean isUser(String user){
+		try {
+			String sqlSelectUsernames = "SELECT username FROM login"
+			
+			ResultSet rs = conn.prepareStatement(sqlSelectUsernames).executeQuery();
+			while (rs.next()){
+				if (user.equals(rs.getString("username"))){
+					rs.close();
+					return true;
+				}
+			}
+			rs.close();
+			return false;
+			
+		} catch (SQLException e){
+			System.err.println(e.toString());
+		}
 	}
 	private String getSHA(String input){
 		try{
